b530939e896f304a82cd4baaa67cd651002ab153
IsGris <homka228tyk@gmail.com>
2024-03-24 18:59:09 +0500

    created inputs

--- Source/DuelShooter/Private/DuelShooterPlayerController.cpp
+++ Source/DuelShooter/Private/DuelShooterPlayerController.cpp
@@ -2,10 +2,14 @@
 
 
 #include "DuelShooterPlayerController.h"
+#include "DuelShooterPlayer.h"
+#include "Components/CapsuleComponent.h"
 
 void ADuelShooterPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
 	PrimaryActorTick.bCanEverTick = false;
+	
+	PawnOwner = Cast<ADuelShooterPlayer>(GetPawn());
 }
--- Source/DuelShooter/Private/DuelShooterPlayerController.h
+++ Source/DuelShooter/Private/DuelShooterPlayerController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "DuelShooterPlayerController.generated.h"
 
+#define JUMP_FORCE 300
+
+class ADuelShooterPlayer;
+
 /**
  * PlayerController for DuelShooter Player
  */
@@ -15,4 +19,10 @@ class DUELSHOOTER_API ADuelShooterPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 	virtual void BeginPlay() override;
+
+protected:
+	TObjectPtr<ADuelShooterPlayer> PawnOwner;
+
+protected:
+	bool IsCrouching = false;
 };
